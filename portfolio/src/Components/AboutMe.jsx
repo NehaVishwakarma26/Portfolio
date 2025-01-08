950d39fde95ecbbc956378bfc77cbf9621e29379
NehaVishwakarma26 <nehasvish186@gmail.com>
2025-01-08 19:47:19 +0530

    about me line editted

--- portfolio/src/Components/AboutMe.jsx
+++ portfolio/src/Components/AboutMe.jsx
@@ -18,7 +18,7 @@ function AboutMe() {
 About Me    </Typography>
     <Grid container spacing={2} marginLeft={2} paddingBottom={10}>
         <Grid item xs={8} sm={4} md={10} sx={{color:'white',fontFamily:'Mona Sans,sans-serif'}} >
-        I'm a third-year Computer Engineering student living in Mumbai, a creative web developer passionate about crafting functional, user-friendly applications. With expertise in the MERN stack, I love bringing innovative projects to life. Let's build something impactful together!
+        I'm a third-year Computer Engineering student living in Mumbai, a creative web developer passionate about crafting functional, user-friendly applications. With expertise in the MERN stack, I love bringing innovative projects to life.
 
 </Grid>
     </Grid>
